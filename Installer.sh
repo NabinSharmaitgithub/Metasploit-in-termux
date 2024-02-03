ae81bd243c40e276af8954b7bdca31621af5d4e1
Nabin_sharma <105613345+NabinSharmaitgithub@users.noreply.github.com>
2024-02-03 15:29:23 +0545

    Add files via upload

---
+++ Installer.sh
@@ -0,0 +1,52 @@
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+echo "
+██╗  ██╗ █████╗ ██╗     ██╗    ██╗     ██╗███╗   ██╗██╗   ██╗██╗  ██╗
+██║ ██╔╝██╔══██╗██║     ██║    ██║     ██║████╗  ██║██║   ██║╚██╗██╔╝
+█████╔╝ ███████║██║     ██║    ██║     ██║██╔██╗ ██║██║   ██║ ╚███╔╝ 
+██╔═██╗ ██╔══██║██║     ██║    ██║     ██║██║╚██╗██║██║   ██║ ██╔██╗ 
+██║  ██╗██║  ██║███████╗██║    ███████╗██║██║ ╚████║╚██████╔╝██╔╝ ██╗
+╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝    ╚══════╝╚═╝╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝
+                                                                        
+"
+center() {
+  termwidth=$(stty size | cut -d" " -f2)
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+echo " Loading..."
+source <(echo "c3Bpbm5lcj0oICd8JyAnLycgJy0nICdcJyApOwoKY291bnQoKXsKICBzcGluICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9uZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBpIGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcciRpIjsKICAgICAgc2xlZXAgMC4yOwogICAgZG9uZTsKICBkb25lCn0KCmNvdW50" | base64 -d)
+
+echo
+center "*** Dependencies installation***"
+
+pkg upgrade -y -o Dpkg::Options::="--force-confnew"
+
+pkg install -y git wget -o Dpkg::Options::="--force-confnew"
+
+apt install proot-distro
+
+clear
+
+echo
+echo "*** Downloading***"
+wget https://raw.githubusercontent.com/NabinSharmaitgithub/Metasploit-in-termux/main/kali.sh?token=GHSAT0AAAAAACNXLFBIFZR3ZQZAUGE5AAREZN6AKFQ -o kali.sh
+
+mv $PREFIX/etc/proot-distro/kali.sh
+
+proot-distro install kali
+
+echo
+echo "*** Installation***"
+
+proot-distro login klinux
+
+echo
+center "*"
+echo -e "\033[32m YOU CAN USE "proot-distro login klinux" TO START KALI LINUX AGAIN 
+
+TO USE KALI LINIX IN GUI USE THE SCRIPT \033[0m"
+center "*"
+
+
+am start -a android.intent.action.VIEW -d "https://github.com/NabinSharmaitgithub"
